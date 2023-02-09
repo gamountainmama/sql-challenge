d432c5f95b9683c82c68edf53b4df22706415637
gamountainmama <gamountainmama@gmail.com>
2023-02-09 17:40:15 -0500

    updated tables with primary keys

--- EmployeeSQL/create_tables.sql
+++ EmployeeSQL/create_tables.sql
@@ -1,52 +1,65 @@
+--Create departments table
+
 DROP TABLE departments;
 
 CREATE TABLE departments(
-	dept_no	VARCHAR(5),
-	dept_name VARCHAR(30));
+	dept_no	VARCHAR(5) NOT NULL PRIMARY KEY,
+	dept_name VARCHAR(30) NOT NULL);
 	
 SELECT * FROM departments;
+
+--Create department employees table
 
 DROP TABLE dept_emp;
 
 CREATE TABLE dept_emp(
-	emp_no INT,	
-	dept_no	VARCHAR(5));
+	id SERIAL PRIMARY KEY,
+	emp_no INT NOT NULL,	
+	dept_no	VARCHAR(5) NOT NULL);
 	
 SELECT * FROM dept_emp;
+
+--Create department managers table
 
 DROP TABLE dept_manager;
 
 CREATE TABLE dept_manager(
-	dept_no	VARCHAR(5),
-	emp_no INT);
+	dept_no	VARCHAR(5) NOT NULL,
+	emp_no INT NOT NULL PRIMARY KEY);
 	
 SELECT * FROM dept_manager;
+
+--Create employees table
 
 DROP TABLE employees;
 
 CREATE TABLE employees(
-	emp_no INT,
-	emp_title_id VARCHAR(10),
+	emp_no INT NOT NULL PRIMARY KEY,
+	emp_title_id VARCHAR(10) NOT NULL,
 	birth_date DATE,	
-	first_name VARCHAR(30),
-	last_name VARCHAR(30),
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
 	sex VARCHAR(1), 
-	hire_date DATE);
+	hire_date DATE NOT NULL);
 	
 SELECT * FROM employees;
+
+--Create salaries table
 
 DROP TABLE salaries;
 
 CREATE TABLE salaries(
-	emp_no INT,
-	salary INT);
+	emp_no INT NOT NULL PRIMARY KEY,
+	salary INT NOT NULL);
 	
 SELECT * FROM salaries;
+
+--Create titles table
 
 DROP TABLE titles;
 
 CREATE TABLE titles(
-	title_id VARCHAR (10),
-	title VARCHAR(30);
+	title_id VARCHAR (10) NOT NULL PRIMARY KEY,
+	title VARCHAR(30) NOT NULL);
 	
 SELECT * FROM titles;
