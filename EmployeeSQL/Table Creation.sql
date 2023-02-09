d637fcdfbc0437f7759f9766726faa2ac452ef5b
gamountainmama <gamountainmama@gmail.com>
2023-02-09 14:35:45 -0500

    updated with drop table commands

--- EmployeeSQL/Table Creation.sql
+++ EmployeeSQL/Table Creation.sql
@@ -1,8 +1,12 @@
+DROP TABLE departments;
+
 CREATE TABLE departments(
 	dept_no	VARCHAR,
 	dept_name VARCHAR);
 	
 SELECT * FROM departments;
+
+DROP TABLE dept_emp;
 
 CREATE TABLE dept_emp(
 	emp_no INT,	
@@ -10,11 +14,15 @@ CREATE TABLE dept_emp(
 	
 SELECT * FROM dept_emp;
 
+DROP TABLE dept_manager;
+
 CREATE TABLE dept_manager(
 	dept_no	VARCHAR,
 	emp_no INT);
 	
 SELECT * FROM dept_manager;
+
+DROP TABLE employees;
 
 CREATE TABLE employees(
 	emp_no INT,
@@ -27,11 +35,15 @@ CREATE TABLE employees(
 	
 SELECT * FROM employees;
 
+DROP TABLE salaries;
+
 CREATE TABLE salaries(
 	emp_no INT,
 	salary INT);
 	
 SELECT * FROM salaries;
+
+DROP TABLE titles;
 
 CREATE TABLE titles(
 	title_id VARCHAR,
